57511ae69565e2a2c3ec396d27e65646bd429a0a
Raida <patricsi2002@gmail.com>
2023-09-11 12:45:27 +0200

    MainBranch egy uj Branchben.

---
+++ MainBranch.java
@@ -0,0 +1,12 @@
+public class Main{
+public static void main(String[] args){
+
+    GreetingsGenerator generator = new GreetingsGenerator();
+System.out.println(generator.generateGreeting("Uram"));
+
+int a=1;
+int b=2;
+System.out.println(a+b);
+
+}
+}
